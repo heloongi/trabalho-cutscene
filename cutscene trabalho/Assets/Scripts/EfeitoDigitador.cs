29eeea3f63e193579b8fca697da439304e080b6b
heloongi <r.heloisa@escolar.ifrn.edu.br>
2024-09-09 08:33:30 -0300

    projeto salvo 1

---
+++ cutscene trabalho/Assets/Scripts/EfeitoDigitador.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class EfeitoDigitador : MonoBehaviour
+{
+   private TextMeshProUGUI componenteTexto;
+    private AudioSource _audioSource;
+}
